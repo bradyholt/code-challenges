0153ce3e4f312cda2cd3e8e7dee47933765e3256
Brady Holt <brady.holt@gmail.com>
2016-09-01 17:02:24 -0500

    Print Fibonacci

---
+++ fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintFibonacciSequence(10);
+        }
+
+        public static void PrintFibonacciSequence(int count)
+        {
+            int a = 0;
+            int b = 1;
+
+            Console.WriteLine(a.ToString());
+
+            for (int i = 0; i <= count; i++){
+                int currentResult = (a + b);
+                Console.WriteLine(currentResult.ToString());
+
+                a = b;
+                b = currentResult;
+            }
+        }
+    }
+}
